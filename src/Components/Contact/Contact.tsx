96c4d0e8acb6acd544e344deaa203b6d1fecbb8c
Cemalettin Taş <cemalettin.tas.metu@gmail.com>
2020-06-19 09:27:34 +0300

    removed photo, changed some paddings, margins

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,21 +1,11 @@
 import './Contact.scss';
 
-import React /* , { useState, useEffect, useContext } */ from 'react';
-// Define props type
+import React from 'react';
 
-/*
-type PropType= {
-  prop1: string;
-  prop2: string;
-};
-*/
-
-// Destructure the props
-const Contact = (/* { prop1, prop2 }: PropType */): JSX.Element => {
+const Contact = (): JSX.Element => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-
   };
 
   return (
--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -2,15 +2,12 @@ import './Home.scss';
 import navMain from 'navMain';
 import Navigation from 'Components/Navigation/Navigation';
 
-import React /* , { useState, useEffect, useContext } */ from 'react';
-import picMid from 'Images/cemo_cropped.png';
+import React from 'react';
 
-// Destructure the props
-const Home = (/* { prop1, prop2 }: PropType */): JSX.Element => {
+const Home = (): JSX.Element => {
   return (
     <div className="Home" id="home">
       <header className="Header">
-        <img className="HeaderPic" src={picMid} alt="cemalettin taş profile" />
         <div className="HeaderSec">
           <h1>I am Cemalettin.</h1>
           <h2>I develop elegant and fast user interfaces</h2>
