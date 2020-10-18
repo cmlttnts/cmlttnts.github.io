e6dd9fe8e34573c202c9b2f302675616d171f48d
Cemalettin Taş <cemalettin.tas.metu@gmail.com>
2020-10-18 12:26:13 +0300

    Add hover effects for github icons

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,8 +1,8 @@
-import './Home.scss';
-import navMain from 'navMain';
-import Navigation from 'Components/Navigation/Navigation';
+import "./Home.scss";
 
-import React from 'react';
+import Navigation from "Components/Navigation/Navigation";
+import React from "react";
+import navMain from "navMain";
 
 const Home = (): JSX.Element => {
   return (
@@ -11,24 +11,24 @@ const Home = (): JSX.Element => {
         <h1>I am Cemalettin.</h1>
         <h2>I develop elegant and fast user interfaces</h2>
         <div className="HeaderSec">
-          <a className="ctaHomeSec" href="#about">About</a>
-          <a className="ctaHomeSec" href="#contact">Contact</a>
+          <a className="ctaHomeSec" href="#about">
+            About
+          </a>
+          <a className="ctaHomeSec" href="#contact">
+            Contact
+          </a>
         </div>
         <div className="SocialLinks">
-          <a
-            href="https://www.linkedin.com/in/cemalettintas/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href="https://www.linkedin.com/in/cemalettintas/" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-linkedin"></i>
           </a>
-          <a
-            href="https://github.com/cmlttnts"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href="https://github.com/cemalettin-simsoft" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-github"></i>
-
+            <span className="tooltip">Work Account</span>
+          </a>
+          <a href="https://github.com/cmlttnts" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-github"></i>
+            <span className="tooltip">Personal Account</span>
           </a>
         </div>
       </header>
