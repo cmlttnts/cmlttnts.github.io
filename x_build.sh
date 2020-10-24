d21953972254e4c29f6c5ce48dbafa7714be322c
Cemalettin Taş <cemalettin.tas.metu@gmail.com>
2020-10-24 23:25:15 +0300

    update cv

--- x_build.sh
+++ x_build.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
-git add .
-git commit -m "$*"
-git push -f
-git push -f githubIo dev
-echo "**********************************pushed"
-yarn run build
-echo "**********************************build done"
-git checkout master
-echo "**********************************now in master branch"
-sleep 1
-rm *.txt
-rm *.json
-rm *.js
-rm *.html
-rm -r static
-echo "**********************************removed old files"
-cp -a build/. .
-git add .
-git commit -m "$*"
-git push -f
-git push -f githubIo master
-echo "**********************************master is pushed"
-git checkout dev
-echo "**********************************return to dev branch"
+git add . \
+&& git commit -m "$*" \
+&& git push -f \
+&& git push -f githubIo dev \
+&& echo "**********************************pushed" \
+&& yarn run build \
+&& echo "**********************************build done" \
+&& git checkout master \
+&& echo "**********************************now in master branch" \
+&& sleep 1 \
+&& rm *.txt \
+&& rm *.json \
+&& rm *.js \
+&& rm *.html \
+&& rm -r static \
+&& echo "**********************************removed old files" \
+&& cp -a build/. . \
+&& git add . \
+&& git commit -m "$*" \
+&& git push -f \
+&& git push -f githubIo master \
+&& echo "**********************************master is pushed" \
+&& git checkout dev \
+&& echo "**********************************return to dev branch"
